30ad0863015011e7f57fe9c4a5ce53930d6c86b5
unknown <george.dvorak@educanet.cz>
2021-10-21 09:18:57 +0200

    Edit

--- src/Cnbprs.java
+++ src/Cnbprs.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Locale;
@@ -22,13 +21,14 @@ public class Cnbprs {
                     }
                 }
 
-                String stateName = currentLine.substring(0, currentLine.indexOf('|'));
-
                 Number numericValue = format.parse(currentLine.substring(separatorIndexes.get(3)+1));
                 double doubleValue = numericValue.doubleValue();
                 double czk = 1 / doubleValue;
-                String code = currentLine.substring(separatorIndexes.get(2)+1, currentLine.lastIndexOf("|"));
                 double value = 1.00 / doubleValue;
+
+                String stateName = currentLine.substring(0, currentLine.indexOf('|'));
+                String code = currentLine.substring(separatorIndexes.get(2)+1, separatorIndexes.get(3));
+
                 System.out.printf("%f %s = 1 CZK\n", value, code);
 
 
